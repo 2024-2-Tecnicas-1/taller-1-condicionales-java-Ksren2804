9ad0c154363ad72e30af41e71535720948a8d540
Ksrenn <karen170bayona@hotmail.com>
2024-09-07 21:18:22 -0500

    Año Bisisesto

---
+++ ProyectosJava/src/main/java/com/mycompany/proyectosjava/ProyectosJava.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectosjava;
+
+/**
+ *
+ * @author User
+ */
+public class ProyectosJava {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
